f0820d79f36d4315046fbef8a7e311b4c4c8cabc
Andrew Hinkson <adh1056@comcast.net>
2024-04-04 15:17:48 -0400

    Structured reciept page better

--- orderReciept.php
+++ orderReciept.php
@@ -1,4 +1,5 @@
 <?php
+echo 'Thank you, '.$_POST['name'].', for your order! \nThe flavors you ordered are: \n';
 $flavors = $_POST['flavor'];
 foreach ($flavors as $flavor)
-    echo($flavor);
+    echo($flavor).'\n';
