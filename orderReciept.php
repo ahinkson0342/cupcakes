33193e26d1d3b189145a7934d85807aaf9288002
Andrew Hinkson <adh1056@comcast.net>
2024-04-04 15:22:53 -0400

    another newline fix

--- orderReciept.php
+++ orderReciept.php
@@ -2,4 +2,4 @@
 echo "Thank you," .$_POST['name']. ", for your order! \nThe flavors you ordered are: \n";
 $flavors = $_POST['flavor'];
 foreach ($flavors as $flavor)
-    echo($flavor).'\n';
+    echo($flavor)."\n";
