da03ca55fd810e399a74b537b890b93dda030905
Andrew Hinkson <adh1056@comcast.net>
2024-04-04 13:41:14 -0400

    added receipt page

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <!-- BOOTSTRAP CDNS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- END BOOTSTRAP CDNS -->
+
+
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -9,18 +17,20 @@
 </head>
 <body>
     <h1>Cupcake Fundraiser</h1>
-    <form action="" method="POST">
-        <p>Your name:</p>
-        <input type="text" name="name">
-        <input type="checkbox" name="flavor[]">The Grasshopper
-        <input type="checkbox" name="flavor[]">Whiskey Maple Bacon
-        <input type="checkbox" name="flavor[]">Carrot Walnut
-        <input type="checkbox" name="flavor[]">Salted Caramel Cupcake
-        <input type="checkbox" name="flavor[]">Red Velvet
-        <input type="checkbox" name="flavor[]">Lemon Drop
-        <input type="checkbox" name="flavor[]">Tiramisu
-        <input type="submit" name="submit">
-    </form>
+    <div class="align-items-center col-6">
+        <form action="orderReciept.php" method="POST" class="w-100">
+            <p>Your name:</p>
+            <input type="text" name="name">
+            <input type="checkbox" name="flavor[]">The Grasshopper
+            <input type="checkbox" name="flavor[]">Whiskey Maple Bacon
+            <input type="checkbox" name="flavor[]">Carrot Walnut
+            <input type="checkbox" name="flavor[]">Salted Caramel Cupcake
+            <input type="checkbox" name="flavor[]">Red Velvet
+            <input type="checkbox" name="flavor[]">Lemon Drop
+            <input type="checkbox" name="flavor[]">Tiramisu
+            <input type="submit" name="submit" value="Order">
+        </form>
+    </div>
 </body>
 </html>
 <?php
---
+++ orderReciept.php
@@ -0,0 +1,4 @@
+<?php
+$flavors = $_POST['flavor'];
+foreach ($flavors as $flavor)
+    print($flavor);
